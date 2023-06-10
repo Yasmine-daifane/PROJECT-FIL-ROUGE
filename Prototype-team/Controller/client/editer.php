29a0eabccfea63eeff2ecde0c41de8cad95355f6
yasmine <daifaney@gmail.com>
2023-06-10 17:56:17 +0100

    push

--- Prototype-team/Controller/client/editer.php
+++ Prototype-team/Controller/client/editer.php
@@ -1,8 +1,8 @@
 <?php
-// define('__ROOT__', dirname(dirname(dirname(__FILE__))));
-// include_once(__ROOT__ . './Managers/GestionClient.php');
-include "../Managers/GestionClient.php";
-$GestionClient = new GestionClients();
+define('__ROOT__', dirname(dirname(dirname(__FILE__))));
+
+include_once(__ROOT__ . '/Managers/GestionClient.php');
+$GestionClients = new GestionClients();
 
 if (isset($_GET['id'])) {
     $client = $GestionClient->RechercherParId($_GET['Id_client']);
@@ -12,8 +12,9 @@ if (isset($_POST['modifier'])) {
     $id = $_POST['Id_client'];
     $nom = $_POST['nom'];
     $email = $_POST['email'];
-    $GestionClient->Modifier($id, $nom, $email);
+    $GestionClients->Modifier($id, $nom, $email);
     header('Location: index.php');
 }
-include_once(__ROOT__ . '/Views/clients/editer.php');
+
+include_once(__ROOT__.'/Views/clients/editer.php')
 ?>
